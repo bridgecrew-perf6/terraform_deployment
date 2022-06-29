dc6357d28f020775abe123a66b259e2022028052
leonmsdev <95130787+leonmsdev@users.noreply.github.com>
2022-06-29 09:44:56 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@
 terraform {
   backend "remote" {
     # The name of your Terraform Cloud organization.
-    organization = "example-organization"
+    organization = "leonms"
 
     # The name of the Terraform Cloud workspace to store Terraform state files in.
     workspaces {
-      name = "example-workspace"
+      name = "terraform_deployment"
     }
   }
 }
